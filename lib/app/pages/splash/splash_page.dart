19ef90842387c5e491b5fdaec9876b38f83fb56f
personal <filipe.boeck@hotmail.com>
2023-01-31 20:48:34 -0300

    criacao da tela de splash

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,7 +1,10 @@
+import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/app_styles.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/colors_app.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
+
+import '../../core/config/env/env.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -9,22 +12,42 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
-      ),
-      body: Column(
-        children: [
-          Container(),
-          DeliveryButton(
-            label: 'Teste label',
-            onPressed: () {},
-            width: 200,
-            height: 200,
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'text'),
-          )
-        ],
+      body: ColoredBox(
+        color: Color(0XFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: context.percentHeigth(.30),
+                  ),
+                  Image.asset(
+                    'assets/images/logo.png',
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  DeliveryButton(
+                    width: context.percentWidth(.6),
+                    label: 'ACESSAR',
+                    onPressed: () {},
+                    height: 35,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
